0042a76b0f1c915c50f5145d9695b0fdf803302a
SabrinaHF <sabrinahoflor@gmail.com>
2024-12-04 02:13:50 -0300

    Atualização

--- conselhos_pra_ze.py
+++ conselhos_pra_ze.py
@@ -28,11 +28,23 @@ def menu_principal():
     print(' 2. Quer relembrar alguns conselhos véi? Apoi escolha esse aqui.')
     print(' 3. Quer ir simbora? Então tá certo...')
     print('\n')
-    opcao = int(input(' -> Me diga aqui a opção, homi: '))
     
+    while True:
+        try:
+            opcao = int(input(' -> Me diga aqui a opção, homi: '))
+            if opcao in [1, 2, 3]:
+                break
+            else:
+                print('Errrouu! Opção inválida. Tente novamente, por favor.')
+        except ValueError:
+            print('Por favor, digite um número válido, vai.')
+    
+    return opcao
+
 menu_principal()
 
-def opcao_um():
+def exibir_conselhos():
+
 
     print('\n')
     num = int(input(' -> Quer um conselho ou mais? Me diga quantos: '))
@@ -48,9 +60,27 @@ def opcao_um():
     print(' -> Ê hein, achei profundo... tu achasse? Me diz lá embaixo.')
     print('\n')
     print(' 1. Entendi foi nada, traduz aí!')
-    print(' 2. Eu entendi foi tudo, pois sou troglodita... digo, poliglota. Guarde esses conselhos pra mim, robô.')
+    print(' 2. Guarde esses conselhos pra mim, robô!')
     print(' 3. Quero voltar lá no começo!')
     print('\n')
     escolha = int(input(' -> Digita aqui ó, a opção:'))
     
-opcao_um()
+def main():
+
+    while True:
+        opcao = menu_principal()
+        
+        if opcao == 1:
+            voltar_ao_menu = exibir_conselhos()
+            if voltar_ao_menu:
+                continue  
+        elif opcao == 2:
+            print("Aqui você pode mostrar conselhos antigos...")
+        elif opcao == 3:
+            print('Saindo... Até mais!')
+            break  
+        else:
+            print('Opção inválida, tente novamente.')
+
+main()
+
